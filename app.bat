4e4e70375686877de4c4c306201d52d01b9d8adf
Tristan Bettany <tristan.bettany@gmail.com>
2021-04-16 21:43:35 +0100

    Setup docker

---
+++ app.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+
+CALL :CASE_%1
+IF ERRORLEVEL 1 CALL :DEFAULT_CASE
+
+ECHO Done!
+EXIT /B
+
+:CASE_install
+    ECHO Removing temporary application files...
+    @RD /S /Q "config_gen/vendor"
+    ECHO Building...
+    docker build -t tristanbettany:confgen .
+    ECHO Install dependancies...
+    docker run -it --rm -v %cd%:/app -w /app/config_gen tristanbettany:confgen composer install
+    GOTO END_CASE
+
+:CASE_phpshell
+    docker run -it --rm -v %cd%:/app -w /app/config_gen tristanbettany:confgen php -a
+    GOTO END_CASE
+
+:CASE_bash
+    docker run -it --rm -v %cd%:/app -w /app/config_gen tristanbettany:confgen bash
+    GOTO END_CASE
+
+:DEFAULT_CASE
+    ECHO Unknown function "%1"
+    GOTO END_CASE
+:END_CASE
+    VER > NUL
+    GOTO :EOF
