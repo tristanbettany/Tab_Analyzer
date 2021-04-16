b33cb38397d49815085505ec9622bedee307c9f8
Tristan Bettany <tristan.bettany@climbcreative.co.uk>
2021-04-16 23:07:53 +0100

    Query php versions from gitlab

--- config_gen/src/Commands/ConfigGenCommand.php
+++ config_gen/src/Commands/ConfigGenCommand.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Commands;
 
+use Gitlab\Exception\RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Gitlab\Client as GitlabClient;
+use Exception;
 
 final class ConfigGenCommand extends Command
 {
@@ -22,6 +25,44 @@ final class ConfigGenCommand extends Command
         InputInterface $input,
         OutputInterface $output
     ) :int {
+
+        $client = new GitlabClient();
+        $client->authenticate($_ENV['GITLAB_TOKEN'], GitlabClient::AUTH_HTTP_TOKEN);
+
+        $projects = $client->groups()->projects(
+            $_ENV['GROUP_ID'],
+            [
+                'include_subgroups' => true,
+                'per_page' => 100,
+                'order_by' => 'updated_at'
+            ]
+        );
+
+        $repos = [];
+        foreach($projects as $project) {
+            try {
+                $branch = $client->repositories()->branch($project['id'], 'master');
+                $composerFile = $client->repositoryFiles()->getRawFile($project['id'], 'composer.json', $branch['commit']['id']);
+            } catch(RuntimeException $e) {
+                // File or branch wasnt found so continue
+                continue;
+            }
+
+            preg_match('/"php":\s"(.+)",/', $composerFile, $matches);
+
+            if (
+                empty($matches) === false
+                && empty($matches[1]) === false
+            ) {
+                $repos[] = [
+                    'id' => $project['id'],
+                    'name' => $project['name'],
+                    'php_version' => $matches[1],
+                ];
+            }
+        }
+
+        var_dump($repos);
 
         return 0;
     }
